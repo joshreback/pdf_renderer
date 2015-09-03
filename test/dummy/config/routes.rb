767334bbfe0bddc697f2b246d96489cbcf6dac81
Josh Reback <joshr@edmodo.com>
2015-09-03 01:40:52 -0400

    added another action & test that specified template is rendered

--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+  get "/home", to: "home#index", as: :home
+  get "/another", to: "home#another", as: :another
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
